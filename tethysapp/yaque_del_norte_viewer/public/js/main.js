faf55c21a79d11cdbb429b494cc1c31977b248b8
wade <waderoberts123@gmail.com>
2019-02-13 14:54:54 -0700

    deleted __pycache__

--- tethysapp/yaque_del_norte_viewer/public/js/main.js
+++ tethysapp/yaque_del_norte_viewer/public/js/main.js
@@ -5,5 +5,4 @@ $(document).ready(function () {
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
 });
