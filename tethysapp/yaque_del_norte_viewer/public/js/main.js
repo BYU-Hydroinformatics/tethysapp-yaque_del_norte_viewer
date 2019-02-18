72a0234f7a2e0e6bddb087309cc8499d66bb8907
wade <waderoberts123@gmail.com>
2019-02-18 12:49:57 -0700

    Attempting to add Yaque Del Norte Layer

--- tethysapp/yaque_del_norte_viewer/public/js/main.js
+++ tethysapp/yaque_del_norte_viewer/public/js/main.js
@@ -4,5 +4,36 @@ $(document).ready(function () {
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    $.ajax({
+        dataType: "json",
+        url: "/static/yaque_del_norte_viewer/GeoJSON/yaque_del_norte.geojson",
+        success: function (data) {
+            console.log(data);
+
+            let myStyle = {
+                "color": "#2427ff",
+                "weight": 5,
+                "opacity": 0.65
+            };
+
+            L.geoJSON(data, {style: myStyle}).addTo(map);
+
+            // let yaqueDelNorteLayer = new L.GeoJSON();
+            //
+            // $(data.features).each(function (key, data) {
+            //
+            //     console.log(data);
+            //     yaqueDelNorteLayer.addData(data);
+            // });
+
+            // console.log(yaqueDelNorteLayer);
+            //
+
+            // yaqueDelNorteLayer.setStyle(myStyle);
+            //
+            // yaqueDelNorteLayer.addTo(map);
+        }
+    });
 });
 
