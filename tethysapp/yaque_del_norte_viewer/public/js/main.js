43b40afde4a2ff914a4b4671c15e0c0891417ae3
wade <waderoberts123@gmail.com>
2019-02-19 21:54:22 -0700

    Added a test layer from geoserver

--- tethysapp/yaque_del_norte_viewer/public/js/main.js
+++ tethysapp/yaque_del_norte_viewer/public/js/main.js
@@ -2,38 +2,17 @@ $(document).ready(function () {
     let map = L.map("map").setView([19.042805, -70.581183], 7.2);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        zIndex: 1,
     }).addTo(map);
 
-    $.ajax({
-        dataType: "json",
-        url: "/static/yaque_del_norte_viewer/GeoJSON/yaque_del_norte.geojson",
-        success: function (data) {
-            console.log(data);
+    L.tileLayer.wms("http://localhost:8080/geoserver/yaque_del_norte/wms", {
+        layers: 'yaque_del_norte:dominican_republic-national-drainage_line',
+        transparent: true,
+        zIndex: 2,
+        format: "image/png",
+    }).addTo(map);
 
-            let myStyle = {
-                "color": "#2427ff",
-                "weight": 5,
-                "opacity": 0.65
-            };
-
-            L.geoJSON(data, {style: myStyle}).addTo(map);
-
-            // let yaqueDelNorteLayer = new L.GeoJSON();
-            //
-            // $(data.features).each(function (key, data) {
-            //
-            //     console.log(data);
-            //     yaqueDelNorteLayer.addData(data);
-            // });
-
-            // console.log(yaqueDelNorteLayer);
-            //
-
-            // yaqueDelNorteLayer.setStyle(myStyle);
-            //
-            // yaqueDelNorteLayer.addTo(map);
-        }
-    });
 });
+
 
