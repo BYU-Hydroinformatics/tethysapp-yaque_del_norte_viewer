3216c08c193f8bbee087876bce54cadd4e289d26
jaredlillywhite <jaredclillywhite@gmail.com>
2019-02-27 16:01:50 -0700

    Added play button.

--- tethysapp/yaque_del_norte_viewer/public/js/main.js
+++ tethysapp/yaque_del_norte_viewer/public/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    let map = L.map("map").setView([19.042805, -70.581183], 7.2);
+    let map = L.map("map", {
+        fullscreenControl: true,
+        timeDimension: true,
+        timeDimensionControl: true}).setView([19.042805, -70.581183], 7.2);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
