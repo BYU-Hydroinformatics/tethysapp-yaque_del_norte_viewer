53d0f9cc061d72c1970b1872f8581008ae9a76cf
wade <waderoberts123@gmail.com>
2019-03-09 14:33:39 -0700

    Simplified the layers and added layer control

--- tethysapp/yaque_del_norte_viewer/public/js/main.js
+++ tethysapp/yaque_del_norte_viewer/public/js/main.js
@@ -1,121 +1,68 @@
-$(document).ready(function () {
-    const map = L.map("map", {
-        fullscreenControl: true,
-        timeDimension: true,
-        timeDimensionControl: true,
-    }).setView([19.042805, -70.581183], 8);
+// Add map and basemap to the screen
+// TODO: Recolor the layers that aren't drainage lines
+const map = L.map("map", {
+    fullscreenControl: true,
+    timeDimension: true,
+    timeDimensionControl: true,
+}).setView([19.042805, -70.581183], 8);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        zIndex: 1,
-    }).addTo(map);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    zIndex: 1,
+}).addTo(map);
 
-    // L.tileLayer.wms("http://localhost:8080/geoserver/yaque_del_norte/wms", {
-    //     layers: 'yaque_del_norte:population_yaque',
-    //     transparent: true,
-    //     zIndex: 2,
-    //     format: "image/png",
-    // }).addTo(map);
+const owsrootUrl = 'http://localhost:8080/geoserver/ows';
 
-    // TODO: Split this stuff into functions, and only allow one layer at a time
-    const owsrootUrl = 'http://localhost:8080/geoserver/ows';
+// Population Layer (populationData variable defined in geojson_layers.js)
+const populationLayer = L.geoJSON(populationData, {
+    onEachFeature: onEachFeaturePopulation
+}).addTo(map);
 
-    // Population Layer
-    const defaultParametersPopulation = {
-        service: 'WFS',
-        version: '2.0',
-        request: 'GetFeature',
-        typeName: 'yaque_del_norte:population_yaque',
-        outputFormat: 'text/javascript',
-        format_options: 'callback:getJson',
-        SrsName: 'EPSG:4326'
-    };
-    const parametersPopulation = L.Util.extend(defaultParametersPopulation);
-    const populationURL = owsrootUrl + L.Util.getParamString(parametersPopulation);
-
-    // 	Land Use Layer
-    const defaultParametersLandUse = {
-        service: 'WFS',
-        version: '2.0',
-        request: 'GetFeature',
-        typeName: 'yaque_del_norte:yaque',
-        outputFormat: 'text/javascript',
-        format_options: 'callback:getJson',
-        SrsName: 'EPSG:4326'
-    };
-    const parametersLandUse = L.Util.extend(defaultParametersLandUse);
-    const landUseURL = owsrootUrl + L.Util.getParamString(parametersLandUse);
-
-    // 	Watershed Layer Layer
-    const defaultParametersWatershed = {
-        service: 'WFS',
-        version: '2.0',
-        request: 'GetFeature',
-        typeName: 'yaque_del_norte:yaque_uso',
-        outputFormat: 'text/javascript',
-        format_options: 'callback:getJson',
-        SrsName: 'EPSG:4326'
-    };
-    const parametersWatershed = L.Util.extend(defaultParametersWatershed);
-    const watershedURL = owsrootUrl + L.Util.getParamString(parametersWatershed);
-
-    // Population Layer
-    $.ajax({
-        url: populationURL,
-        dataType: 'jsonp',
-        jsonpCallback: 'getJson',
-        success: function (response) {
-            let WFSLayer = L.geoJson(response, {
-                // style: function (feature) {
-                //     return {
-                //         stroke: false,
-                //         fillColor: 'FFFFFF',
-                //         fillOpacity: 0
-                //     };
-                // },
-                onEachFeature: onEachFeaturePopulation  // Helper Function
-            }).addTo(map);
-        }
-    });
-
-    // Land Use Layer
-    $.ajax({
-        url: landUseURL,
-        dataType: 'jsonp',
-        jsonpCallback: 'getJson',
-        success: function (response) {
-            let WFSLayer = L.geoJson(response, {
-                style: {
-                    weight: 2,
-                    color: "#999",
-                    opacity: 1,
-                    fillColor: "#B0DE5C",
-                    fillOpacity: 0.8
-                },
-                onEachFeature: onEachFeatureLandUse  // Helper Function
-            }).addTo(map);
-        }
-    });
-
-    // $.ajax({
-    //     url: URL,
-    //     dataType: 'jsonp',
-    //     jsonpCallback: 'getJson',
-    //     success: function (response) {
-    //         let WFSLayer = L.geoJson(response, {
-    //             // style: function (feature) {
-    //             //     return {
-    //             //         stroke: false,
-    //             //         fillColor: 'FFFFFF',
-    //             //         fillOpacity: 0
-    //             //     };
-    //             // },
-    //             onEachFeature: onEachFeaturePopulation  // Helper Function
-    //         }).addTo(map);
-    //     }
-    // });
-
+// 	Watershed Layer (watershedData variable defined in geojson_layers.js)
+const watershedLayer = L.geoJSON(watershedData, {
+    onEachFeature: onEachWatershed
 });
+
+// Drainage Line Layer
+// const defaultParameters = {
+//     service: 'WFS',
+//     version: '2.0',
+//     request: 'GetFeature',
+//     typeName: 'yaque_del_norte:ClippedYaqueDrainage',
+//     outputFormat: 'text/javascript',
+//     format_options: 'callback:getJson',
+//     SrsName: 'EPSG:4326'
+// };
+//
+// const parameters = L.Util.extend(defaultParameters);
+// const URL = owsrootUrl + L.Util.getParamString(parameters);
+//
+// $.ajax({
+//     url : URL,
+//     dataType : 'jsonp',
+//     jsonpCallback : 'getJson',
+//     success : function (response) {
+//         console.log(JSON.stringify(response));
+//         let WFSLayer = L.geoJson(response).addTo(map);
+//     }
+// });
+const drainageLineLayer = L.geoJSON(drainageLineData).addTo(map);
+
+// 	Land Use Layer Layer
+const landUseLayer = L.tileLayer.wms(owsrootUrl, {
+    layers: 'yaque_del_norte:yaque_uso',
+    transparency: 'true',
+    format: 'image/png',
+    opacity: 0.5,
+});
+
+let overlays = {
+    "Population Layer": populationLayer,
+    "Watershed Layer": watershedLayer,
+    "Land Use Layer": landUseLayer,
+};
+
+L.control.layers(overlays).addTo(map);
 
 
 $("#dateinput").on('change', function () {
@@ -148,14 +95,10 @@ function onEachFeaturePopulation(feature, layer) {
             </tbody>
         </table>`;
 
-    // if (feature.properties && feature.properties.popupContent) {
-    //     popupContent += feature.properties.popupContent;
-    // }
-
     layer.bindPopup(popupContent);
 }
 
-function onEachFeatureLandUse(feature, layer) {
+function onEachWatershed(feature, layer) {
     let popupContent = `
         <table>
             <tbody>
@@ -194,9 +137,6 @@ function onEachFeatureLandUse(feature, layer) {
             </tbody>
         </table>`;
 
-    // if (feature.properties && feature.properties.popupContent) {
-    //     popupContent += feature.properties.popupContent;
-    // }
-
     layer.bindPopup(popupContent);
 }
+
